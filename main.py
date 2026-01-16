2d24853741795250e0716a9067e2e83593db69a8
John Pinto <159508244+johndv47@users.noreply.github.com>
2026-01-16 07:46:15 -0700

    documentation update

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+'''
+script: investment_return.py
+action: a. request user input for investment time horizion.
+        b. perform interest calculation.
+        c. print calculation.
+author: John Pinto
+date: 1/16/2026
+'''
+
 n = int(input("Enter the number of years: "))
 r = 0.07
 p = 1000
