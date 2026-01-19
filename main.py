457de2332e14aa5f178fca7fcb81ea1cd2bda151
John Pinto <159508244+johndv47@users.noreply.github.com>
2026-01-19 09:25:15 -0700

    Refactor investment return calculation for multiple years

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 '''
 script: investment_return.py
-action: a. request user input for investment time horizion.
-        b. perform interest calculation.
-        c. print calculation.
+action: a. loops through years 10, 20, and 30 to perform interest calculation.
+        b. prints calculation.
+note: p(1+r)^n
+      p = principal
+      r = rate
+      n = years
 author: John Pinto
-date: 1/16/2026
+date: 1/19/2026
 '''
+print("Welcome to the investment return calculator!\n")
 
-n = int(input("Enter the number of years: "))
+years = [10, 20, 30]
 r = 0.07
 p = 1000
-a = p * (1+r)**n
-print(a)
+
+for n in years:
+    a = p * (1+r)**n
+    print(f"After {n} years, your principal amount (${p}), will be ${a:.2f}.")
